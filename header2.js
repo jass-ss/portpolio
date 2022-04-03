59377695028505611b87d673eb1f94f2d9295795
jass--ss <jsmsue010@gmail.com>
2022-04-04 04:53:10 +0900

    비디오 추가

--- header2.js
+++ header2.js
@@ -1,46 +1,52 @@
 const fixed = document.querySelector('.fixed');
 const pics = document.querySelectorAll('.pic');
+const videos =document.querySelectorAll('video');
 console.log(pics);
 
 fixed.addEventListener('mouseenter',()=>{
     pics.forEach(p=>{
         p.style.flex = '1';
+        p.querySelector('video').style.height = '15%'
     })
 })
 
-/*pics.forEach((p)=>{
-    p.addEventListener('mouseenter',()=>{
-        pics.forEach(p=>{
-            p.style.flex='1';
-            p.querySelector('img').style.height ='50vh';    
-        });
-        p.style.flex = '4';
-        p.querySelector('img').style.height = '85vh';
+videos.forEach(v=>{
+    v.addEventListener('mouseenter',(e)=>{
+        e.currentTarget.play();
     })
-}) */
+})
 
 pics.forEach((p,index)=>{
-    p.addEventListener('mouseenter',()=>{
-        const img = p.querySelector('img');
+    p.addEventListener('mouseenter',(e)=>{
+       // const img = p.querySelector('img');
+
         pics.forEach(p=>{
             p.style.flex='1';
             p.querySelector('img').style.height = '50vh'
+            p.querySelector('video').style.height = '15%'
         });
+
+        e.currentTarget.querySelector('video').play();
         p.style.flex = '4';
+
+        //img.style.height = '85vh';
+        //pics[index].querySelector('img').style.height = '85vh';
+
+        //.pic을 각각 설정
         if(index === 0){
-            img.style.height ='85vh';
+            pics[index].querySelector('img').style.height = '85vh';
+            pics[index].querySelector('video').style.height = '70vh';
+        }  
+        if(index === 1){
+            pics[index].querySelector('img').style.height = '85vh';
+            pics[index].querySelector('video').style.height = '70vh';
+        }  
+        if(index === 2){
+            pics[index].querySelector('img').style.height = '85vh';
+            pics[index].querySelector('video').style.height = '70vh';
         }
-        if(index===1){
-            img.style.height ='85vh';
-        }
-        if(index===2){
-            img.style.top = 0;
-            img.style.height ='85vh';
-            //pics[1].querySelector('img').style.bottom ='10vh';
-            //pics[1].querySelector('img').style.height ='75vh';
-        }else{
-            pics[2].querySelector('img').style.top = '10%';
-            pics[2].querySelector('img').style.height = '65vh';
-        }
+    });
+    p.addEventListener('mouseleave',(e)=>{
+        e.currentTarget.querySelector('video').pause();
     })
-})
+}); 
